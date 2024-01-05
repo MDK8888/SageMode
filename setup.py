ac653a546f4137a045f646578580fc2039f0235c
MDK8888 <kending19@gmail.com>
2024-01-04 16:10:21 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+def parse_requirements(file_path):
+    with open(file_path, 'r') as f:
+        return [line.strip() for line in f if line.strip()]
+
+setup(
+    name='sagemode',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt')
+)
