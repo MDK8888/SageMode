8c8cf7d2ace5b31033ae68033c1d9b9129a85ad9
MDK8888 <kending19@gmail.com>
2024-01-04 16:29:27 -0800

    changed to latin-1 in setup decoding of requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 def parse_requirements(file_path):
-    with open(file_path, 'r', encoding="utf-8") as f:
+    with open(file_path, 'r', encoding="latin-1") as f:
         return [line.strip() for line in f if line.strip()]
 
 setup(
