443bcf91fce5e2d283c39f4e4c8c885688b40081
MDK8888 <kending19@gmail.com>
2024-01-14 19:48:20 -0500

    changed examples for 0.2.0

--- Examples/hf_test.py
+++ Examples/hf_test.py
@@ -1,6 +1,11 @@
-from sagemode.ResourceUser.HFSageMakerResourceUser import HFSageMakerResourceUser
+from sagemode.ResourceUser import HFSageMakerResourceUser
 
-user = HFSageMakerResourceUser("ml.g5.xlarge")
-user.deploy("google/flan-t5-small", "FlanT5Lambda")
+user = HFSageMakerResourceUser()
 
-user.use({"text":"Hello, how are you?", "parameters": None})
+user.deploy(model_id="google/flan-t5-small", instance_type="ml.g5.xlarge", skips=["compression", "download", "upload"], lambda_function_name="FlanT5Lambda")
+
+response = user.use({"text": "Hello, how are you?", "parameters":None})
+
+print("Flan-T5 response:", response)
+
+user.teardown()
