548747d9f2e69ca7d910572ec8f997eba09c6161
MDK8888 <kending19@gmail.com>
2024-01-19 15:36:57 -0500

    fixed speculative decode

--- SageMode/GPTFast/GPTFast.py
+++ SageMode/GPTFast/GPTFast.py
@@ -2,12 +2,11 @@ from torch import nn
 from Compile import torch_compile_model
 from KVCache import modify_transformer_attention_blocks
 from Quantize import quantize_model
-from SpeculativeDecode import add_speculative_decode
+from SpeculativeDecode import speculative_decode
 
 def gpt_fast(model:nn.Module, **transformer_kwargs):
     model = torch_compile_model(model)
     model = modify_transformer_attention_blocks(model, **transformer_kwargs)
     model = quantize_model(model)
-    model = add_speculative_decode(model)
-
+    model.speculative_decode = speculative_decode
     return model
--- SageMode/GPTFast/SpeculativeDecode.py
+++ SageMode/GPTFast/SpeculativeDecode.py
@@ -8,55 +8,63 @@ from typing import Callable
 #2. Run Mp on prefix and prefix + x1 concurrently to get the distributions for x2 and p(x).
 #3. If x1 is rejected by Mp, reject and resample x1 from an altered distribution, otherwise keep x1 and x2. 
 
-def add_speculative_decode(model:nn.Module):
+def speculative_decode(
+    self,
+    draft_model:nn.Module,
+    cur_tokens:torch.Tensor,
+    speculate_k:int,
+    model_decode_function:Callable,
+    draft_model_decode_function:Callable,
+    sampling_function:Callable,
+    kv_cache:bool = False,
+    **kwargs
+) -> torch.Tensor:
+    device = cur_tokens.device
 
-    def speculative_decode(
-        self,
-        draft_model:nn.Module,
-        input_pos:int,
-        x:torch.Tensor,
-        speculate_k:int,
-        model_decode_function:Callable,
-        draft_model_decode_function:Callable,
-        sampling_function:Callable,
-        **sampling_kwargs
-    ) -> torch.Tensor:
-        device = x.device
+    draft_model_sampling_kwargs = kwargs["draft_model_decoding_kwargs"]
 
-        draft_model_sampling_kwargs = sampling_kwargs["draft_model_sampling_kwargs"]
-        draft_tokens, draft_prob = draft_model_decode_function(**draft_model_sampling_kwargs)
+    if kv_cache:
+        decode_input = torch.Tensor([cur_tokens[-1]]) #the KV cache if implemented correctly only needs the most recent token.
+    else:
+        decode_input = cur_tokens
 
-        if type(draft_tokens) == list:
-            draft_tokens = torch.cat(draft_tokens)
-        
-        model_sampling_kwargs = sampling_kwargs["model_sampling_kwargs"]
-        model_tokens, model_prob = model_decode_function(x, draft_tokens, **model_sampling_kwargs)
-        
-        if type(model_tokens) == list:
-            model_tokens = torch.cat(model_tokens)
-        
-        p = draft_prob[torch.arange(0, speculate_k, device=device), draft_tokens]
-        q = model_prob[torch.arange(0, speculate_k, device=device), draft_tokens]
+        draft_tokens, draft_prob = draft_model_decode_function(decode_input, speculate_k, **draft_model_sampling_kwargs)
 
-        accept_draft_prob = torch.minimum(torch.ones(()), q[:speculate_k]/ p)
-        rejected_locations = (torch.rand_like(accept_draft_prob) > accept_draft_prob).nonzero()
-
-        if rejected_locations.shape[0] == 0:
-            last_token = sampling_function(model_prob[-1])
-            draft_model.forward(
-                x=draft_tokens, 
-                start_pos=input_pos
-            )
-            return torch.cat([draft_tokens, last_token])
-        else:
-            accept_length = rejected_locations[0].item()
-            p = draft_prob[accept_length]
-            q = model_prob[accept_length]
-            new = q - p
-            new = torch.where(new > 0, new, 0.0)
-            next_token = sampling_function(new)
-            return torch.cat([draft_tokens[:accept_length], next_token])
+    if type(draft_tokens) == list:
+        draft_tokens = torch.cat(draft_tokens)
     
-    model.speculative_decode = speculative_decode
+    model_sampling_kwargs = kwargs["model_decoding_kwargs"]
+    model_prob = model_decode_function(decode_input, speculate_k+1, **model_sampling_kwargs)
 
-    return model
+    assert len(model_prob) == len(draft_prob) + 1, "In order for speculative decoding to work, the main model must sample one more token than the draft model."
+    
+    p = model_prob[torch.arange(0, speculate_k, device=device), draft_tokens]
+    q = draft_prob[torch.arange(0, speculate_k, device=device), draft_tokens]
+
+    ratio = p / q
+    rand = torch.rand_like(ratio)
+    n = torch.argmax(rand > ratio).item()
+
+    if n < len(ratio) and rand[n] > ratio[n]:
+        p = draft_prob[n]
+        q = model_prob[n]
+        new = q - p
+        new = torch.where(new > 0, new, 0.0)
+        last_token = sampling_function(new)
+        if kv_cache:
+            assert hasattr(self, "rollback_cache"), "Error: In order for speculative decoding to work with a kv cache, you must be able to update it."
+            self.rollback_cache(n + len(cur_tokens) + 1)
+            assert hasattr(draft_model, "rollback_cache"), "Error: In order for speculative decoding to work with a kv cache, you must be able to update it."
+            draft_model.rollback_cache(n + len(cur_tokens) + 1)
+        
+        return torch.cat([draft_tokens[:n+1], last_token])
+    else: #we accept all tokens from the draft model
+        last_token = sampling_function(model_prob[-1])
+        if kv_cache:
+            assert hasattr(self, "rollback_cache"), "Error: In order for speculative decoding to work with a kv cache, you must be able to update it."
+            self.rollback_cache(n + len(cur_tokens) + 2)
+            assert hasattr(draft_model, "rollback_cache"), "Error: In order for speculative decoding to work with a kv cache, you must be able to update it."
+            draft_model.rollback_cache(n + len(cur_tokens) + 1)
+
+        #assume that draft_model already has a kv cache attached.
+        return torch.cat([draft_tokens, last_token])
